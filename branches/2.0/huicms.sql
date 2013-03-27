0308d0aa893f46b5ff4c84dd9bc772f3e0d739f4
terry.wang1991@gmail.com <terry.wang1991@gmail.com@57871ae0-5732-f07d-abeb-6683711b6cc8>
2013-03-27 17:46:47 +0000

    提交

--- branches/2.0/huicms.sql
+++ branches/2.0/huicms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2013 年 03 月 26 日 09:19
+-- 生成日期: 2013 年 03 月 28 日 01:46
 -- 服务器版本: 5.5.29
 -- PHP 版本: 5.4.6-1ubuntu1.2
 
@@ -31,19 +31,26 @@ CREATE TABLE IF NOT EXISTS `hui_admin` (
   `u_name` varchar(50) NOT NULL COMMENT '管理员用户名',
   `u_passwd` varchar(32) NOT NULL COMMENT '管理员密码',
   `u_ip` varchar(15) NOT NULL COMMENT '管理员登陆IP',
+  `u_photo` varchar(100) DEFAULT NULL COMMENT '管理员头像',
+  `u_username` varchar(50) DEFAULT NULL COMMENT '姓名',
+  `u_sex` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '性别 0：保密 1：男 2：女',
+  `u_phone` varchar(11) NOT NULL COMMENT '手机',
+  `u_email` varchar(50) NOT NULL COMMENT 'Email',
+  `u_qq` varchar(13) DEFAULT NULL COMMENT 'QQ',
+  `u_description` text COMMENT '描述',
   `u_countlog` int(11) NOT NULL COMMENT '登陆次数',
   `u_lastlogin_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '最后登陆时间',
   `u_create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
   `u_update_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '更新时间',
   PRIMARY KEY (`u_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='管理员' AUTO_INCREMENT=2 ;
 
 --
 -- 转存表中的数据 `hui_admin`
 --
 
-INSERT INTO `hui_admin` (`u_id`, `u_name`, `u_passwd`, `u_ip`, `u_countlog`, `u_lastlogin_time`, `u_create_time`, `u_update_time`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '127.0.0.1', 28, '2013-03-26 09:19:12', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `hui_admin` (`u_id`, `u_name`, `u_passwd`, `u_ip`, `u_photo`, `u_username`, `u_sex`, `u_phone`, `u_email`, `u_qq`, `u_description`, `u_countlog`, `u_lastlogin_time`, `u_create_time`, `u_update_time`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '127.0.0.1', '/Public/upload/photo/515321a73e88d.jpg', '王辉', 1, '13817918575', 'shuaige@52sum.com', '466209365', '', 52, '2013-03-28 01:13:17', '0000-00-00 00:00:00', '2013-03-28 00:43:19');
 
 -- --------------------------------------------------------
 
@@ -58,14 +65,38 @@ CREATE TABLE IF NOT EXISTS `hui_admin_log` (
   `log_create` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '登陆时间',
   `log_ip` varchar(15) NOT NULL COMMENT '登陆IP',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='管理员日志' AUTO_INCREMENT=46 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='管理员日志' AUTO_INCREMENT=70 ;
 
 --
 -- 转存表中的数据 `hui_admin_log`
 --
 
 INSERT INTO `hui_admin_log` (`id`, `u_id`, `u_name`, `log_create`, `log_ip`) VALUES
-(45, 1, 'admin', '2013-03-26 09:19:12', '127.0.0.1');
+(45, 1, 'admin', '2013-03-26 09:19:12', '127.0.0.1'),
+(46, 1, 'admin', '2013-03-26 09:49:42', '127.0.0.1'),
+(47, 1, 'admin', '2013-03-26 10:17:12', '127.0.0.1'),
+(48, 1, 'admin', '2013-03-26 10:31:40', '127.0.0.1'),
+(49, 1, 'admin', '2013-03-26 10:54:49', '127.0.0.1'),
+(50, 1, 'admin', '2013-03-26 11:02:52', '127.0.0.1'),
+(51, 1, 'admin', '2013-03-26 11:11:38', '127.0.0.1'),
+(52, 1, 'admin', '2013-03-26 11:30:13', '127.0.0.1'),
+(53, 1, 'admin', '2013-03-26 11:31:47', '127.0.0.1'),
+(54, 1, 'admin', '2013-03-26 11:33:47', '127.0.0.1'),
+(55, 1, 'admin', '2013-03-26 14:14:22', '127.0.0.1'),
+(56, 1, 'admin', '2013-03-26 20:00:24', '127.0.0.1'),
+(57, 1, 'admin', '2013-03-26 21:05:14', '127.0.0.1'),
+(58, 1, 'admin', '2013-03-26 21:05:36', '127.0.0.1'),
+(59, 1, 'admin', '2013-03-26 22:13:32', '127.0.0.1'),
+(60, 1, 'admin', '2013-03-27 01:28:33', '127.0.0.1'),
+(61, 1, 'admin', '2013-03-27 09:10:42', '127.0.0.1'),
+(62, 1, 'admin', '2013-03-27 13:28:21', '127.0.0.1'),
+(63, 1, 'admin', '2013-03-27 20:19:25', '127.0.0.1'),
+(64, 1, 'admin', '2013-03-27 21:30:28', '127.0.0.1'),
+(65, 1, 'admin', '2013-03-27 22:34:50', '127.0.0.1'),
+(66, 1, 'admin', '2013-03-27 22:44:34', '127.0.0.1'),
+(67, 1, 'admin', '2013-03-27 22:44:55', '127.0.0.1'),
+(68, 1, 'admin', '2013-03-27 22:58:58', '127.0.0.1'),
+(69, 1, 'admin', '2013-03-28 01:13:17', '127.0.0.1');
 
 -- --------------------------------------------------------
 
@@ -90,7 +121,7 @@ CREATE TABLE IF NOT EXISTS `hui_config` (
 --
 
 INSERT INTO `hui_config` (`c_id`, `c_module`, `c_key`, `c_value`, `c_value_desc`, `c_create_time`, `c_update_time`) VALUES
-(1, 'ADMIN_ACCESS', 'EXPIRED_TIME', '3600', '登陆超时时间', '2013-03-25 16:43:42', '0000-00-00 00:00:00'),
+(1, 'ADMIN_ACCESS', 'EXPIRED_TIME', '3600', '登陆超时时间', '2013-03-26 03:12:33', '0000-00-00 00:00:00'),
 (2, 'ADMIN_ACCESS', 'SYS_ADMIN', 'admin', '系统管理员', '2013-03-25 16:43:50', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
