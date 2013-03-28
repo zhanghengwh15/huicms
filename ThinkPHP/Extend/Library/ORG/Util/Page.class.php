8c7794fd37ab4385decf2f3841f0b824be067bce
terry.wang1991@gmail.com <terry.wang1991@gmail.com@57871ae0-5732-f07d-abeb-6683711b6cc8>
2013-03-28 05:53:36 +0000

    PHP

--- ThinkPHP/Extend/Library/ORG/Util/Page.class.php
+++ ThinkPHP/Extend/Library/ORG/Util/Page.class.php
@@ -146,5 +146,85 @@ class Page {
             array($this->config['header'],$this->nowPage,$this->totalRows,$this->totalPages,$upPage,$downPage,$theFirst,$prePage,$linkPage,$nextPage,$theEnd),$this->config['theme']);
         return $pageStr;
     }
+    
+    public function newshow() {
+        if(0 == $this->totalRows) return '';
+        $p              =   $this->varPage;
+        $nowCoolPage    =   ceil($this->nowPage/$this->rollPage);
+        //echo "<pre>";print_r($this->config);exit;
+        // 分析分页参数
+        if($this->url){
+            $depr       =   C('URL_PATHINFO_DEPR');
+            $url        =   rtrim(U('/'.$this->url,'',false),$depr).$depr.'__PAGE__';
+        }else{
+            if($this->parameter && is_string($this->parameter)) {
+                parse_str($this->parameter,$parameter);
+            }elseif(empty($this->parameter)){
+                unset($_GET[C('VAR_URL_PARAMS')]);
+                if(empty($_GET)) {
+                    $parameter  =   array();
+                }else{
+                    $parameter  =   $_GET;
+                }
+            }
+            $parameter[$p]  =   '__PAGE__';
+            $url            =   U('',$parameter);
+        }
+        //上下翻页字符串
+        $upRow          =   $this->nowPage-1;
+        $downRow        =   $this->nowPage+1;
+        if ($upRow>0){
+            $upPage     =   "<li><a href='".str_replace('__PAGE__',$upRow,$url)."'>".$this->config['prev']."</a></li>";
+        }else{
+            $upPage     =   '';
+        }
+
+        if ($downRow <= $this->totalPages){
+            $downPage   =   "<li><a href='".str_replace('__PAGE__',$downRow,$url)."'>".$this->config['next']."</a></li>";
+        }else{
+            $downPage   =   '';
+        }
+        // << < > >>
+        if($nowCoolPage == 1){
+            $theFirst   =   '';
+            $prePage    =   '';
+        }else{
+            $preRow     =   $this->nowPage-$this->rollPage;
+            $prePage    =   "<li><a href='".str_replace('__PAGE__',$preRow,$url)."' >上".$this->rollPage."页</a>";
+            $theFirst   =   "<li><a href='".str_replace('__PAGE__',1,$url)."' >".$this->config['first']."</a></li>";
+        }
+        if($nowCoolPage == $this->coolPages){
+            $nextPage   =   '';
+            $theEnd     =   '';
+        }else{
+            $nextRow    =   $this->nowPage+$this->rollPage;
+            $theEndRow  =   $this->totalPages;
+            $nextPage   =   "<li><a href='".str_replace('__PAGE__',$nextRow,$url)."' >下".$this->rollPage."页</a></li>";
+            $theEnd     =   "<li><a href='".str_replace('__PAGE__',$theEndRow,$url)."' >".$this->config['last']."</a></li>";
+        }
+        // 1 2 3 4 5
+        $linkPage = "";
+        for($i=1;$i<=$this->rollPage;$i++){
+            $page       =   ($nowCoolPage-1)*$this->rollPage+$i;
+            if($page!=$this->nowPage){
+                if($page<=$this->totalPages){
+                    $linkPage .= "<li><a href='".str_replace('__PAGE__',$page,$url)."'>&nbsp;".$page."&nbsp;</a></li>";
+                }else{
+                    break;
+                }
+            }else{
+                if($this->totalPages != 1){
+                    $linkPage .= "<li class='page'><a href='javascript:void(0);'>".$page."</span></li>";
+                }
+            }
+        }
+//        echo "<pre>";print_r($pageStr);exit;
+        $pageStr     =   str_replace(
+            array('%header%','%nowPage%','%totalRow%','%totalPage%','%upPage%','%downPage%','%first%','%prePage%','%linkPage%','%nextPage%','%end%'),
+            array($this->config['header'],$this->nowPage,$this->totalRows,$this->totalPages,$upPage,$downPage,$theFirst,$prePage,$linkPage,$nextPage,$theEnd),$this->config['theme']);
+        
+        return $pageStr;
+    }
+
 
 }
