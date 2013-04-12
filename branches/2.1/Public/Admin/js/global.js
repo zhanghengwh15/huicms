853f3f3e98fcc359033d3a427fe58fa6b72b9d52
terry.wang1991@gmail.com <terry.wang1991@gmail.com@57871ae0-5732-f07d-abeb-6683711b6cc8>
2013-04-12 07:10:19 +0000

    Huicms2.1版本分支

--- branches/2.1/Public/Admin/js/global.js
+++ branches/2.1/Public/Admin/js/global.js
@@ -5,9 +5,9 @@ function setFrameworkLayout() {
     window_width = $(window).width();
     window_height = $(window).height();
     //设置头部
-    $('.header').css({
+    $('.head').css({
         'width':window_width-0,
-        'height':'70px'
+        'height':'91px'
     });
     var content_height = window_height-0;
     $('.contentBox').css({
@@ -15,36 +15,31 @@ function setFrameworkLayout() {
     }).height(content_height).css({
         'float':'left'
     });
-    $('.sidebar').width(225).height(content_height-14).css({
+    $('.sidebar').width(161).height(content_height-14).css({
         'float':'left',
         'overflow':'hidden'
     });
-    $('.leftside').width(225).height(content_height-80).css({
+    $('.leftside').width(161).height(content_height-0).css({
         'float':'left',
         'overflow':'hidden'
     });
-    var right_width = window_width - 220;
-    $('.breadcrumb').width(right_width-40).height(35).css({
-        'float':'left'
-    });
-    $('.rightside').width(right_width-45).height(content_height-102).css({
-        'float':'left',
+    var right_width = window_width - 141;
+    $('.rightcontent').width(right_width-25).height(content_height-102).css({
+        'float':'right',
         'overflow-x':'hidden',
         'overflow-y':'auto'
     });
-    $('#sliderNavBox').width(225).height(content_height-250).css({
+    $('#sliderNavBox').width(161).height(content_height-100).css({
         'float':'left',
         'overflow-y':'auto',
-        'overflow-x':'hidden',
-        'margin-top':'10px'
+        'overflow-x':'hidden'
     });
 }
-
 $(function() {
-    $('body, .main').css({
+    $('body').css({
         'padding':0,
         'margin':0,
-        'float':'left',
+        'float':'none',
         'width':'100%',
         'overflow':'hidden'
     });
@@ -52,120 +47,50 @@ $(function() {
     framework_layout_timeout_id = window.setInterval('setFrameworkLayout()', 500);
 })
 
-/**
- * 鼠标悬浮在左侧导航，上下移动菜单
- * @author Terry <admin@52sum.com>
- * @date 2013-03-27
- */
-//$(document).ready(function(){
-//    var settime;
-//    $("#sliderNavBox").mousemove(function(e){
-//        var mTop = (($("#sliderNavBox").height() - $("#sliderNavBoxInner").height() ) / $("#sliderNavBox").height())*(e.pageY - $("#sliderNavBox").position().top);
-//        mTop = (mTop>0)?0:mTop;
-//        $('#sliderNavBoxInner').css("margin-top",mTop);
-//    });
-//    $("#sliderNavBox").hover(function(){
-//        clearTimeout(settime);
-//    },function(){
-//        settime = setTimeout(function(){
-//            $('#sliderNavBoxInner').animate({
-//                marginTop:0
-//            }, 'normal');
-//        },10000);
-//    });
-//});
-
-/**
- * 表单验证插件设置
- * @author Terry<admin@52sum.com>
- * @date 2013-03-27
- */
-$(document).ready(function(){
-    $.metadata.setType("attr","validate");
-});
-
-$(document).ready(function(){   
-    $(".formvalidate").validate({
-        errorElement: "span",
-        errorClass: "errormsg"
-    });   
+$(document).ready(function () {
+    $('span.bar-btn').click(function () {
+        $('ul.bar-list').toggle('fast');
+    });
 });
 
 $(document).ready(function(){
     $('.list tr:odd').addClass('odd');  
-	$('.list tr:even').addClass('even');
+    $('.list tr:even').addClass('even');
     $('.list tr').hover( 
-		function(){
-			$(this).addClass('activity'); 
-		},
-		function(){
-			$(this).removeClass('activity'); 
-		}
-	);
+        function(){
+            $(this).addClass('activity'); 
+        },
+        function(){
+            $(this).removeClass('activity'); 
+        }
+        );
 });
 
-/**
- * 全选与取消全选
- * 将全选的checkbox的class设为checkAll.列表中的checkbox的class设为checkSon
- * @author Terry<admin@52sum.com>
- * @date 2013-03-28
- */
 $(document).ready(function(){
-    /*全选与取消*/
-    $('#checkboxall').click(function(){
-        if($(this).attr('checked')=='checked'){
-            $('.checkSon').attr('checked','checked');
-        }else{
-            $('.checkSon').removeAttr('checked');
-        }
-    });
+    $('.title2div').hover(
+        function(){
+            title = this.title;
+            this.title = '';
+            $(this).css('cursor','pointer'); 
+            $('body').append('<div id="titlediv_jq" class="title_div rouned">'+title+'</div>');
+            var win_width = document.documentElement.clientWidth ? document.documentElement.clientWidth : (window.innerWidth ? window.innerWidth : (document.body.clientWidth ? document.body.clientWidth : 1024));
+            var width = $('#titlediv_jq').width();
+            var x = $(this).offset().left;
+            var left = x < (win_width/2) ? x+$(this).width() : x-width;
+            var top = $(this).offset().top + $(this).height(); 
+            $('#titlediv_jq').css({
+                'top':top+"px",
+                "left":left+"px",
+                "opacity":'0'
+            });
+            $('#titlediv_jq').animate({
+                opacity:'0.9'
+            },600); 
+        },
+        function(){
+            this.title = title; 
+            $('#titlediv_jq').remove(); 
+        }		
+        );
     
-    $(".module-item").change(function(){
-        var parent = $(this).parent().parent().parent().parent();
-        if(this.checked)
-        {
-            $('.select-all,.action-item',parent).attr({
-                'disabled':true,
-                'checked':false
-            });
-        }
-        else
-        {
-            $('.select-all,.action-item',parent).attr({
-                'disabled':false
-            });
-        }
-    });
-    $(".select-all").change(function(){
-        var parent = $(this).parent().parent().parent();
-        if(this.checked)
-        {
-            $('.action-item',parent).attr({
-                'checked':true
-            });
-        }
-        else
-        {
-            $('.action-item',parent).attr({
-                'checked':false
-            });
-        }
-    });
-
-    $(".action-item").change(function(){
-        var parent = $(this).parent().parent().parent();
-        if($(".action-item:not([checked])",parent).length == 0)
-        {
-            $('.select-all',parent).attr({
-                'checked':true
-            });
-        }
-        else
-        {
-            $('.select-all',parent).attr({
-                'checked':false
-            });
-        }
-    });
-    
-})
+});
