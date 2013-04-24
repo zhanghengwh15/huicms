b8fb9f3e14cd18de9917278447fbf77b72fe2cce
terry.wang1991@gmail.com <terry.wang1991@gmail.com@57871ae0-5732-f07d-abeb-6683711b6cc8>
2013-04-24 16:53:47 +0000

    组合商品菜单

---
+++ Lib/Action/Home/AboutAction.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 前台展厅控制器基类
+ *
+ * @package Action
+ * @subpackage Home
+ * @stage 1.0
+ * @author Terry <admin@52sum.com>
+ * @date 2013-04-15
+ */
+class AboutAction extends Action{
+    
+    /**
+     * 暂用为捐赠我们
+     */
+    public function index(){
+        
+    }
+    
+    
+    
+    
+}
