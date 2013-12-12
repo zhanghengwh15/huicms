76e510bc0674f871200afa5969785678c80506de
terry.wang1991@gmail.com <terry.wang1991@gmail.com@57871ae0-5732-f07d-abeb-6683711b6cc8>
2013-12-12 17:04:04 +0000

    会员Icon

--- branches/2.1/Lib/Action/Admin/MemberAction.class.php
+++ branches/2.1/Lib/Action/Admin/MemberAction.class.php
@@ -45,7 +45,8 @@ class MemberAction extends AdminAction{
      * @date 2013-06-01
      */
     public function addMember(){
-        
+        $ary_mLevel = D("MemberLevel")->where(array('ml_status'=>'1'))->select();
+        $this->assign("mLevel",$ary_mLevel);
         $this->display();
     }
     
@@ -59,8 +60,9 @@ class MemberAction extends AdminAction{
         if(!empty($ary_get['mid']) && isset($ary_get['mid'])){
             $data = D($this->name)->where(array('m_id'=>$ary_get['mid']))->find();
             $city_data = D("City")->getCityLastInfo($data['c_id']);
+            $ary_mLevel = D("MemberLevel")->where(array('ml_status'=>'1'))->select();
+            $this->assign("mLevel",$ary_mLevel);
             $this->assign("data",$data);
-//            echo "<pre>";print_r($city_data);exit;
             $this->assign("region",$city_data);
             $this->display();
         }else{
@@ -75,25 +77,9 @@ class MemberAction extends AdminAction{
      */
     public function doSaveMember(){
         $ary_post = $this->_post();
-        if(empty($ary_post['m_id']) && !isset($ary_post['m_id'])){
+        $ary_member = D($this->name)->where(array('m_id'=>$ary_post['m_id']))->find();
+        if(empty($ary_member) && !is_array($ary_member)){
             $this->error("该会员不存在");
-        }
-        if(!empty($ary_post['m_pic']) && isset($ary_post['m_pic'])){
-            $ary_post['m_pic'] = str_replace("/Public/Lib/ueditor/php/../../../", "", str_replace('//',"/",$ary_post['m_pic']));
-        }
-        $photo = $_FILES['m_pic']['name'];
-        if(!empty($photo)){
-            import('ORG.Net.UploadFile');
-            $upload = new UploadFile();     // 实例化上传类
-            $upload->maxSize  = 3145728 ;// 设置附件上传大小
-            $upload->allowExts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-            $upload->savePath =  './Public/upload/photo/';// 设置附件上传目录
-            if(!$upload->upload()) {// 上传错误提示错误信息
-				$this->error($upload->getErrorMsg());
-			}else{// 上传成功 获取上传文件信息
-				$info =  $upload->getUploadFileInfo();
-				$ary_post['m_pic'] = '/Public/upload/photo/' . $info[0]['savename'];
-			}
         }
         
         if(md5($ary_post['m_passwd']) != md5($ary_post['confirm_password'])){
@@ -107,15 +93,74 @@ class MemberAction extends AdminAction{
         }
         $mid = $ary_post['m_id'];
         unset($ary_post['m_id']);
-        $ary_result = D($this->name)->where(array('m_id'=>$mid))->data($ary_post)->save();
-        if(FALSE !== $ary_result){
-            if(!empty($ary_post['u_photo']) && isset($ary_post['u_photo'])){
-                session("pic",$ary_post['u_photo']);
+        $ary_post['m_enteruser'] = isset($ary_post['m_enteruser']) ? '1' : '0';
+        $c_id = '';
+        if(empty($ary_post['region1'])){
+            if(empty($ary_post['city'])){
+                $c_id = $ary_post['province'];
+            }else{
+                $c_id = $ary_post['city'];
+                
             }
+        }else{
+            $c_id = $ary_post['region1'];
+            
+        }
+        unset($ary_post['region1']);
+        unset($ary_post['city']);
+        unset($ary_post['province']);
+        $ary_post['c_id'] = $c_id;
+        $ary_post['m_update_time'] = date("Y-m-d H:i:s");
+        $ary_result = D($this->name)->where(array('m_id'=>$mid))->data($ary_post)->save();
+//        echo "<pre>";print_r(D($this->name)->getLastSql());exit;
+        if(FALSE !== $ary_result){
             $this->success("会员更新成功");
         }else{
             $this->error("会员更新失败");
         }
+    }
+    
+    /**
+     * 新增会员信息
+     * @author Terry<admin@huicms.cn>
+     * @date 2013-12-12
+     */
+    public function doAdd(){
+        $ary_post = $this->_post();
+        if(md5($ary_post['m_passwd']) != md5($ary_post['confirm_password'])){
+            $this->error("两次密码不一致");
+        }
+        unset($ary_post['confirm_password']);
+        if(!empty($ary_post['m_passwd'])){
+            $ary_post['m_passwd']   = md5(trim($ary_post['m_passwd']));
+        }else{
+            unset($ary_post['m_passwd']);
+        }
+        $ary_post['m_enteruser'] = isset($ary_post['m_enteruser']) ? '1' : '0';
+        $c_id = '';
+        if(empty($ary_post['region1'])){
+            if(empty($ary_post['city'])){
+                $c_id = $ary_post['province'];
+            }else{
+                $c_id = $ary_post['city'];
+                
+            }
+        }else{
+            $c_id = $ary_post['region1'];
+            
+        }
+        unset($ary_post['region1']);
+        unset($ary_post['city']);
+        unset($ary_post['province']);
+        $ary_post['c_id'] = $c_id;  
+        $ary_post['m_reg_time']  = date("Y-m-d H:i:s");
+        $ary_result = D($this->name)->add($ary_post);
+        if(FALSE !== $ary_result){
+            $this->success("会员新增成功",U('Admin/Member/index'));
+        }else{
+            $this->error("会员新增失败");
+        }
+        
     }
     
     /**
@@ -185,6 +230,38 @@ class MemberAction extends AdminAction{
         }
         echo json_encode(array("status" => true, "data" => $array_result,'sql'=>D("City")->getLastSql(), "message" => "success"));
         exit;
+    }
+    
+    /**
+     * 校验会员名称是否重复
+     * @author Terry<admin@huicms.cn>
+     * @date 2013-05-28
+     */
+    public function checkName(){
+        $ary_get = $this->_get();
+        if(!empty($ary_get['m_name']) && isset($ary_get['m_name'])){
+            if(!empty($ary_get['m_id']) && isset($ary_get['m_id'])){
+                $where = array();
+                $where['name'] = $ary_get['m_name'];
+                $where['m_id'] = array("neq",$ary_get['m_id']);
+                $ary_result = D($this->name)->where($where)->find();
+                if(!empty($ary_result) && is_array($ary_result)){
+                    $this->ajaxReturn("会员名称已存在");
+                }else{
+                    $this->ajaxReturn(true);
+                }
+            }else{
+                $ary_result = D($this->name)->where(array('m_name'=>$ary_get['m_name']))->find();
+                if(!empty($ary_result) && is_array($ary_result)){
+                    $this->ajaxReturn("会员名称已存在");
+                }else{
+                    $this->ajaxReturn(true);
+                }
+            }
+        }else{
+            $this->ajaxReturn("会员名称不能为空");
+        }
+        
     }
     
 }
